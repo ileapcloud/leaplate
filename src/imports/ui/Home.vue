ae3b947e555dc9084562f574e183bde3ebc593d5
Howard Tian <tianhu@outlook.com>
2018-08-11 09:59:18 +0800

    Change some text on the home page.

--- src/imports/ui/Home.vue
+++ src/imports/ui/Home.vue
@@ -3,9 +3,9 @@
     <p>
       <img src="/vue%2Bmeteor.png"/>
     </p>
-    <p>Work in progress... <a href="https://github.com/Akryum/vue-meteor-demo">Contribute!</a></p>
+    <p>Work in progress... <a href="https://github.com/leapai/leapai_app_map">Contribute!</a></p>
     <p>This application is a complete demo project using Meteor and Vue. It features routing and SSR, and more (vuex, apollo, accounts...) will be added soon.</p>
-    <p><a href="https://github.com/Akryum/vue-meteor/" target="_blank">More info</a></p>
+    <p><a href="https://github.com/leapai/leapai_app_map" target="_blank">More info</a></p>
 
     <p>{{ counter }}</p>
     <p><button @click="onClick">Increment</button></p>
